791057d9d3041294a829bc5ad9d0d8a1d92b1893
jlianacastillo <jcastilloa@ucundinamarca.edu.co>
2024-02-12 16:29:30 -0500

    Cambios

---
+++ EstructuraMultiple.py
@@ -0,0 +1,7 @@
+numero = input(input("Digite un numero:"))
+if numero > 0:
+    print("El numero es positivo")
+elif numero == 0:
+        print("El numero es cero")
+else:
+        print("El numero es negativo")
